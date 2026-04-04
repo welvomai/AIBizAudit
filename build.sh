44360fa1b7df819bd3a9c364db826bf83d1663fe
nithu-04 <nithyashree.s2022@vitstudent.ac.in>
2026-04-04 12:43:03 +0530

    using render

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+apt-get install -y python3-cffi python3-brotli libpango-1.0-0 libpangoft2-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
+pip install -r requirements.txt
